fda161c3d711f06429779f3717df599db543d1de
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-15 22:15:43 +0700

    [master]: Initial app using github repo

---
+++ src/components/mainLayout/MainLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from "@mui/material";
+
+const MainLayout = () => {
+  return (
+    <Container maxWidth>
+    
+    </Container>
+  );
+};
+
+export default MainLayout;
