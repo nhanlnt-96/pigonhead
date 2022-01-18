095ab5774b4cb9a66073712d257cc9ae14615536
nhanlnt-96 <nhanlnt@hotmail.com>
2022-01-19 00:06:05 +0700

    [master]: Update roadmap icon and banner

--- src/configs/roadmapData.js
+++ src/configs/roadmapData.js
@@ -1,26 +1,29 @@
 export const roadmapData = [
   {
-    percent: "10%",
-    content: ["One lucky winner will receive 1 Ethereum, along with a new accessory to create a unique look to their" +
-    " existing Pigeonhead.", "An additional 5 token holders will also receive a new or different accessory airdropped to them to add to their collection."]
+    percent: "10% Website Launch and Ready to Hatch",
+    content: ["One lucky winner will receive 1 Ethereum, along with a new accessory to create a unique look to their existing Pigeonhead.", "An additional 5 token holders will also receive a new or different accessory airdropped to them to add to their collection."]
   },
   {
-    percent: "25%",
+    percent: "25% Pre-Launch Giveaways",
     content: ["Keep up with the flock in the Discord channel and the Pigeonheads with the most engagement will be" +
-    " eligible to receive 2 Ethereum rewards.", "5 new item will also drop to 5 lucky token holders to accessorize their look.\n"]
+    " eligible to receive 2 Ethereum rewards.", "5 new item will also drop to 5 lucky token holders to accessorize their look.","We will announce at least 20 giveaways.we will pick random participants from our\n" +
+    "Discord and Twitter and reward them with their own Pigeonhead on the Launch day. anyone can win."]
   },
   {
-    percent: "50%",
-    content: ["To celebrate our halfway mark, we will be making a $20,000 charity donation to Its from the Sole, a non-for-profit organization focused on collecting, refurbishing and giving away shoes to the homeless in New York City and other metropolitan areas."]
+    percent: "50% Lets show some appreciation",
+    content: ["To celebrate our halfway mark, and show our appreciation, we will be making a $20,000 charity donation to Its from the Sole, a non-for-profit organization focused on collecting, refurbishing and giving away shoes to the homeless in New York City and other metropolitan areas, as well as a $20,000 donation to InspireBag, a community driven organization focused on the vitality of educational preparedness for all children."]
   },
   {
-    percent: "75%",
-    content: ["One-of-One Merchandise", "Our merchandise store will be opened to the Pigeonhead community, where you’ll be able to purchase your own NFT design on hoodies, hats and other items. A portion of the sales will go to the community wallet."]
+    percent: "75% One-of-one Merchandise",
+    content: ["Our merchandise store will be opened to the Pigeonhead community, where you’ll be able to purchase" +
+    " your own NFT design on hoodies, hats and other items. A portion of the sales will go to the community wallet.", "Community wallet launch.","Round trip for two to New York City and sneaker shopping spree at Stadium Goods\n" +
+    "for holders that own 5 pigeons or more."]
   },
   {
-    percent: "100%",
-    content: ["Pigeonhead Collection launches with limited drops each week.", "- We will be releasing new items weekly to add to your Pigeonhead closet for you to\n" +
-    "flex or resell it on Opensea.", "- Our giveaways intensify with the first cold wallet drop in New York City, including one-\n" +
-    "of-a-kind items, Ethereum rewards and much more."]
+    percent: "100% We’ve arrived!",
+    content: ["Pigeonhead Collection launches with limited drops each week.", "- We will be releasing new items" +
+    " weekly to add to your Pigeonhead closet for you to flex or resell it on Opensea.","- Our giveaways intensify with the first cold wallet drop in New York City, including one-\n" +
+    "of-a-kind items, up to 5 Ethereum and much more.","- Geodrops are unlocked, Murals with embedded AR will be unveiled in New York, LA\n" +
+    "and Miami. Murals will be auctioned as NFTs.","- Join us on Discord, Twitter and Instagram!"]
   }
 ];
